9c6e16960444f9e8aaf48c20e69446ba518b54c9
Yuri <+>
2024-02-15 20:39:42 -0500

    My Travel Project

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const myPlaces = document.getElementById('travel-guide')
-
-fetch('http://localhost:3000/favDestinations')
-.then(response => response.json())
-.then(favDestination => {
-    favDestination.forEach(travel => {
+ //function addCountries() {
+    const myPlaces = document.getElementById('travel-guide')
+    fetch('http://localhost:3000/favDestinations')
+    .then(response => response.json())
+    .then(favDestination => {
+        favDestination.forEach(travel => {
         const imgElement = document.createElement('img')
         imgElement.src = travel.image
         myPlaces.appendChild(imgElement)
+        })
     })
-})
+ //}
+//  function countriesClick() {
+//     myplaces.addEventListener('click', (event))
+//     event.
+//  }
 
-//function addCountries(travel) {
-    //const nameOfCountry = document.createElementById('country-details')
-    //nameOfCountry.src = travel.name
-    //const imgElement = document.createElementById('img')
-    //imgElement.src = travel.image
-    //myPlaces.appendChild(imgElement)
-//}
-//function displayTravel()
-//const travelForm = document.getElementById
-//suggestCountry.addEventListener('submit', (event) => {
-   // event.preventDefault()
+ //function main() {
+  //  addCountries()
+ //}
 
-   // const country = document.getElementById()
-   // const country = document.getElementById()
-    //const country = documente.getElementById()
-//})
+//     const nameOfCountry = document.createElementById('country-details')
+//     nameOfCountry.src = travel.name
+//     const imgElement = document.createElementById('img')
+//     imgElement.src = travel.image
+//     //myPlaces.appendChild(imgElement)
+// //}
+// //function displayTravel()
+// //const travelForm = document.getElementById
+// //suggestCountry.addEventListener('submit', (event) => {
+//    // event.preventDefault()
 
-
-
-
+//    // const country = document.getElementById()
+//    // const country = document.getElementById()
+//     //const country = documente.getElementById()
+// //})
+ //}
