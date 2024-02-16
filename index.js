bcf3b24e29c956fbc1990059315e56dfeb089e8b
Yuri <+>
2024-02-16 01:47:00 -0500

    My Travel Project

--- index.js
+++ index.js
@@ -6,26 +6,26 @@
         favDestination.forEach(travel => {
         const imgElement = document.createElement('img')
         imgElement.src = travel.image
-        const textElement = document.createElement("span")
-        textElement.textContent = travel.name
         myPlaces.appendChild(imgElement)
-        })
-    })
- }
-//  function countriesClick() {
-//     myplaces.addEventListener('click', (event))
-//     event.
-//  }
 
-function countriesSubmit(travel) {
-    const countryForm = document.getElementById('user-submit')
-    countryForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const submitText = document.createElement('name')
-    submitText.id = travel.name
+        // let countryDetails = document.getElementById('country-details')
+        // countryDetails.addEventListener('click', addCountries() {
+        // })
+     })
     })
 }
 
+ function countriesClick() {
+   
+  }
+
+function countriesSubmit() {
+    const countryForm = document.getElementById('user-submit')
+    countryForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    countryForm.textContent = 'Form submitted'
+     })
+}
 
  function getDetails() {
     addCountries()
@@ -33,20 +33,3 @@ function countriesSubmit(travel) {
  }
 
  getDetails()
-
-//     const nameOfCountry = document.createElementById('country-details')
-//     nameOfCountry.src = travel.name
-//     const imgElement = document.createElementById('img')
-//     imgElement.src = travel.image
-//     //myPlaces.appendChild(imgElement)
-// //}
-// //function displayTravel()
-// //const travelForm = document.getElementById
-// //suggestCountry.addEventListener('submit', (event) => {
-//    // event.preventDefault()
-
-//    // const country = document.getElementById()
-//    // const country = document.getElementById()
-//     //const country = documente.getElementById()
-// //})
- //}
