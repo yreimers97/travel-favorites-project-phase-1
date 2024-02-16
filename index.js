63d39d1b449926a0c07b1d9cd1143214c4670721
Yuri <+>
2024-02-15 22:59:30 -0500

    My Travel Project

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
- //function addCountries() {
+ function addCountries() {
     const myPlaces = document.getElementById('travel-guide')
     fetch('http://localhost:3000/favDestinations')
     .then(response => response.json())
     .then(favDestination => {
         favDestination.forEach(travel => {
         const imgElement = document.createElement('img')
+        imgElement.src = travel.name
         imgElement.src = travel.image
         myPlaces.appendChild(imgElement)
         })
     })
- //}
+ }
 //  function countriesClick() {
 //     myplaces.addEventListener('click', (event))
 //     event.
 //  }
 
- //function main() {
-  //  addCountries()
- //}
+function countriesSubmit() {
+    const countryForm = document.getElementById('user-submit')
+    countryForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const submitText = document.createElement('name')
+    submitText.id = submit.name
+    })
+}
+
+
+ function main() {
+    addCountries()
+ }
+
+ main()
 
 //     const nameOfCountry = document.createElementById('country-details')
 //     nameOfCountry.src = travel.name
